026284d145db1b2abff1916e23e3d88dd3a72f96
ShahinaSk <shahinashaik2504@gmail.com>
2020-10-25 13:05:28 +0530

    refactored Main.java, createdREADME.md

---
+++ README.md
@@ -0,0 +1,20 @@
+##### Problem Statement 1-(Main.java):
+Write any Java program that will have the following statement as one of the lines in the code. It should not be part of a comment or String. It should be a valid Java Expression. Program does not have to read or print anything, it should compile and run without errors.
+      
+      a.b.c = c.b.a;
+    
+##### Problem Statement 2-(FolderScanner.java):
+Write a program to scan a given folder and print the counts of files grouped by created month(i.e the month in which it is created, like Jan, Feb etc)
+1) Write the example output of the above program first.
+2) Then write the actual implementation.
+
+###### Sample Output:
+~~~~
+Jan	1
+Feb	4
+Mar	4
+Apr	6
+May	4
+Jun	4
+Jul	1
+Dec	2
--- src/com/nityainc/Main.java
+++ src/com/nityainc/Main.java
@@ -1,23 +1,31 @@
 package com.nityainc;
 
-/*Java program to which contains a.b.c = c.b.a; as a statement using static classes*/
+/*Java program to which contains a.b.c = c.b.a; as a statement*/
 public class Main {
 
     public static void main(String[] args) {
+        A a = new A();
+        a.b = new A.B();
+        C c = new C();
+        c.b = new C.B();
+        c.b.a = 15;
+
         a.b.c = c.b.a;
+
         System.out.println(a.b.c);
     }
 
-    private static class a {
-        private static class b {
-            private static int c;
+    private static class A {
+        B b;
+        private static class B {
+            private int c;
         }
     }
 
-    private static class c {
-        private static class b {
-            private static int a = 5;
+    private static class C {
+        B b;
+        private static class B {
+            private static int a;
         }
     }
-
 }
